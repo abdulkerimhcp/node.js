ebae62e167f5677de3a6409314ad507e645c269b
intimeFront <intimeinfo.api@gmail.com>
2021-07-17 22:30:00 +0300

    Node.js example link yönlendirme v.0.1

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const http = require('http') 
+const http = require('http')
 
 const hostName = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url)
-    res.statusCode = 200
-    res.setHeader('Content-Type','text/plain')
-    res.end('Hello Node.js')
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        return res.end('HOME PAGE')
+    }
+    else if (req.url === '/about') {
+        return res.end('ABOUT PAGE')
+    }
+    else if (req.url === '/contact') {
+        return res.end('CONTACT PAGE')
+    } 
+    else {
+        res.statusCode = 404
+        res.end('SAYFA BULUNAMADI')
+    }
 })
 
 
-server.listen(port,hostName,()=>{
+server.listen(port, hostName, () => {
     console.log(`Server Çalışıyor, http://${hostName}:${port}`)
 })
