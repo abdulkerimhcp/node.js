5f08862ddd2f592b974cbaf92975a56e6b46ebda
intimeFront <intimeinfo.api@gmail.com>
2021-07-17 22:41:21 +0300

    Node.js example sayfalama  v.0.2

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const http = require('http')
+const fs = require('fs')
 
 const hostName = '127.0.0.1'
 const port = 3000
 
+const indexPage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const _404Page = fs.readFileSync('404.html')
+
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        return res.end('HOME PAGE')
+        return res.end(indexPage)
     }
     else if (req.url === '/about') {
-        return res.end('ABOUT PAGE')
+        return res.end(aboutPage)
     }
     else if (req.url === '/contact') {
-        return res.end('CONTACT PAGE')
+        return res.end(contactPage)
     } 
     else {
         res.statusCode = 404
-        res.end('SAYFA BULUNAMADI')
+        res.end(_404Page)
     }
 })
 
